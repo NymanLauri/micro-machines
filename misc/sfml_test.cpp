8ca4ab62de0e8db91e45d686de770d3ce5b08416
Räbinä Henry <rabinah1@niksula.hut.fi>
2017-11-12 16:55:21 +0200

    First experiment with Box2D. This version creates a rectangle to which the user can apply forces to make it move.

--- misc/sfml_test.cpp
+++ misc/sfml_test.cpp
@@ -1,12 +1,14 @@
 /* This is a short SFML test file for messing around, not intended to be used in the
  * project.
  * Compiled by typing:
- * g++ -g -Wall -o test sfml_test.cpp -lsfml-graphics -lsfml-window -lsfml-system 
+ * g++ -g -Wall -o test sfml_test.cpp -lsfml-graphics -lsfml-window -lsfml-system -IBox2D -L<path-to-libBox2D.a> -lBox2D -std=c++11
+ * For example, <path-to-libBox2D.a> could be /home/henry/micro-machines-5/misc
  */
 
 #include <iostream>
 #include <cmath>
 #include <SFML/Graphics.hpp>
+#include "Box2D/Box2D.h"
 #define PI 3.14159265
 
 int main() {
@@ -23,19 +25,46 @@ int main() {
     // Move the triangle to the center coordinates of the window.
     shape.setPosition(500,500);
     shape.setFillColor(sf::Color::Green);
+    window.setFramerateLimit(200); // Set fps to be 200.
+
+    const float SCALE = 30.f;
+
+
+    sf::Texture boxTexture; // This is a texture used to draw a Box2D-object to the window.
+    boxTexture.loadFromFile("Enemy.png");
+    b2Vec2 gravity(0.f, 0.0f); // Define gravity, in this case it will be zero.
+    b2World world(gravity); // Define world.
+    b2BodyDef myBodyDef; // Define body.
+    myBodyDef.type = b2_dynamicBody; // Define the body to be dynamic, that is, we can apply force to it.
+    myBodyDef.position.Set(10,5); // The initial position of the body.
+    myBodyDef.angle = 0; // The initial angle of the body.
+    b2Body* dynamicBody = world.CreateBody(&myBodyDef); // Here we create the body.
+    b2PolygonShape boxShape;
+    boxShape.SetAsBox(10,10); // Define a shape that represents a box.
+    b2FixtureDef boxFixtureDef;
+    boxFixtureDef.shape = &boxShape;
+    boxFixtureDef.density = 1;
+    boxFixtureDef.friction = 2.0f;
+    dynamicBody->CreateFixture(&boxFixtureDef);
 
     // Initialize a clock for measuring fps.
-    sf::Clock clock;
-    float fps;
+    //sf::Clock clock;
+    //float fps;
 
     // Main program loop.
     while (window.isOpen()) {
-        fps = 1.0/clock.getElapsedTime().asSeconds();
-        clock.restart();
+      world.Step(1/60.f, 8, 3); // This is used to simulate the world.
+      //fps = 1.0/clock.getElapsedTime().asSeconds();
+      //clock.restart();
         // sf::Event holds information about every event (a key press, mouse movement etc.)
         // which occurs during the program loop.
         sf::Event event;
         // Event loop handles all events which have occurred.
+	sf::Sprite sprite; // Create a sprite.
+	sprite.setTexture(boxTexture);
+	sprite.setOrigin(1.f,1.f);
+	sprite.setPosition(SCALE*dynamicBody->GetPosition().x, SCALE*dynamicBody->GetPosition().y); // Set the position of the sprite to be the position of the Box2D rectangle.
+	
         while (window.pollEvent(event)) {
             switch (event.type) {
                 case sf::Event::Closed:
@@ -49,7 +78,7 @@ int main() {
                             break;
                         case sf::Keyboard::I:
                             // Print information to console for debugging purposes when I is pressed.
-                            std::cout << "fps: " << fps << std::endl;
+                            //std::cout << "fps: " << fps << std::endl;
                             std::cout << "rot: " << shape.getRotation() << std::endl;
                             std::cout << "x: " << shape.getPosition().x << ", y: " << shape.getPosition().y << std::endl;
                         default:
@@ -69,17 +98,45 @@ int main() {
             // Only allow rotations when up or down is pressed at the same time, since a
             // car can only turn when it is moving.
             shape.move(sin(shape.getRotation()*PI/180),-cos(shape.getRotation()*PI/180));
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) shape.rotate(-0.5);
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) shape.rotate(0.5);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	      {
+		shape.rotate(-0.5);
+	      }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	      {
+		shape.rotate(0.5);
+	      }
         }   
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) { 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
             shape.move(-0.5*sin(shape.getRotation()*PI/180),0.5*cos(shape.getRotation()*PI/180));
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) shape.rotate(-0.5);
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) shape.rotate(0.5);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	      {
+		shape.rotate(-0.5);
+	      }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	      {
+		shape.rotate(0.5);
+	      }
         }
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	{
+	    dynamicBody->ApplyForce(b2Vec2(0,-50), dynamicBody->GetWorldCenter(), 1); // Apply force to the body.
+        }
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+	    dynamicBody->ApplyForce(b2Vec2(0,50), dynamicBody->GetWorldCenter(), 1);
+        }
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	{
+	  dynamicBody->ApplyForce(b2Vec2(-50,0), dynamicBody->GetWorldCenter(), 1);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+	  dynamicBody->ApplyForce(b2Vec2(50,0), dynamicBody->GetWorldCenter(), 1);
+	}
         // Clear previously displayed window, draw the new position of the triangle to
         // the window and display the newly drawn window.
         window.clear();
+	window.draw(sprite);
         window.draw(shape);
         window.display();
     }
