9b8277f96422488bccbdcae71f78287777dd7a9b
jkorvuo <juuso.korvuo@aalto.fi>
2017-11-28 13:20:13 +0200

    Removed binaries and redundant files.

--- src/Functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-#include <SFML/Graphics.hpp>
-
-int OptionsWindow(sf::Event e, sf::RenderWindow *w, sf::Font f);
-int StartWindow(sf::Event e, sf::RenderWindow *w, sf::Font f);
-
-#endif
